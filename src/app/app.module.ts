9a3ee6f99200e1a85cf4ede6fb348228bf138d35
Christopher Richardson <49372253+cy3e@users.noreply.github.com>
2022-09-01 16:31:05 -0400

    qr, more pages and more routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
+//qr
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+
+
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
@@ -10,7 +14,10 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy,  useClass: IonicRouteStrategy,  }],
   bootstrap: [AppComponent],
 })
+
+
 export class AppModule {}
+
--- src/app/manager/manager.page.ts
+++ src/app/manager/manager.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-manager',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManagerPage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
 
+  tools(){
+    this.router.navigate(['/tools'])
+  } 
 }
