af56ba2435fe05e7b17dfc63f949c91164ea5566
Luis071 <82769401+Luis071@users.noreply.github.com>
2022-09-08 12:14:05 -0400

    cambios

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -21,14 +21,30 @@ export class HomePage {
     await alert.present();
   }
 
+  enter(){
+    const input = document.getElementById('codigomaestro') as HTMLInputElement | null;
+    const code = input?.value;
+    var cod = code.split(" ").join("");
+
+    if (cod == '1234') {
+      this.router.navigate(['main'])
+      input.value = null;
+    }
+    else {
+      this.err()
+    }
+  }
+
   onKeydown(event) {
       if(event.key === "Enter")
       {
         const input = document.getElementById('codigomaestro') as HTMLInputElement | null;
         const code = input?.value;
+        var cod = code.split(" ").join("");
 
-        if (code == '1234') {
+        if (cod == '1234') {
           this.router.navigate(['main'])
+          input.value = null;
         }
         else {
           this.err()
